daa5ae38ab166abe98842be13b729502911843af
Hin-Tak Leung <htl10@users.sourceforge.net>
2011-02-23 23:43:07 +0000

    Re-add plotUncertainty, read.beagle, read.impute, read.mach removed earlier. Also squelched 6 white space changes.
    Revert "Revert "This version corrects several small bugs and implements storage of uncertain", r48737"
    
    This reverts commit 9b3b3fdb53fd9c818ec2664d51b82759d48d9d7f.
    
    Conflicts:
    
    	man/snpMatrix-package.Rd
    
    Revert "Revert "Some small corrections to recent changes", r49490"
    
    This reverts commit 52898a071baa54d8bcddab1f6e325d933f57721d.
    
    Revert "re-apply small bug-fix/enhancement for named arguments in snp.estimates.glm"
    
    This reverts commit 486d03eeea324115816d145923d3d7fed192b9a8.
    
    Revert "Revert "Remove reference to mvtest.R in collate field (to be included in next release)", r49663"
    
    This reverts commit b882dda749fa0644ea5e4232be7e25cc9ac96092.
    
    Revert "Revert "Fix important bug in read.beagle and typo in imputation vignette causing ", r49940"
    
    This reverts commit c7ecb48d030a9e4fb5e0165a32951bfed17b80d9.
    
    Revert "Revert "Fix bug in imputation vignette (try again!)", r50031"
    
    This reverts commit c4134b4798e60172bd723bc2dbf0bac11f83fdd2.
    
    --amend
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/chopsticks@53212 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/indata.R
+++ R/indata.R
@@ -19,3 +19,13 @@ read.snps.long <-function(files, sample.id=NULL, snp.id=NULL, female=NULL,
         simplify, verbose, in.order, as.integer(every), PACKAGE="snpMatrix")
 }
 
+read.mach <- function(file, colnames=NULL, nrow=NULL) {
+  .Call("read_mach", file, colnames, nrow, PACKAGE="snpMatrix")
+}
+
+read.impute <- function(file, rownames=NULL, nsnp=NULL, snpcol=2) {
+  .Call("read_impute", file, rownames, nsnp, snpcol, FALSE, PACKAGE="snpMatrix")
+}
+read.beagle <- function(file, rownames=NULL, nsnp=NULL, header=TRUE) {
+  .Call("read_impute", file, rownames, nsnp, 0, header, PACKAGE="snpMatrix")
+}
