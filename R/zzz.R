f1102e80f7c5cea81101669dd5b4dc804569d1e6
Hin-Tak Leung <htl10@users.sourceforge.net>
2011-10-04 17:18:02 +0000

    Try once more to make R 2.14 happy
    Conflicts:
    
    	R/zzz.R
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/chopsticks@58766 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,8 +1,12 @@
-.onLoad <- function(lib, pkg) {
-  library.dynam("chopsticks", package=pkg)
+.onLoad <- function(libname, pkgname) {
+  library.dynam("chopsticks", pkgname, libname)
   methods:::bind_activation(TRUE)
 }
 
 .Last.lib <- function(libname, package) {
   methods:::bind_activation(FALSE)
+}
+
+.onUnload <- function(libpath) {
+  library.dynam.unload("chopsticks", libpath)
 }
